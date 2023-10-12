30e5d78df24cc6ebba7e4a7dc9b3a10daa777f2d
SubhamMurarka <subham.murarkag@gmail.com>
2023-10-12 15:08:39 +0530

    completed_part1

--- main.go
+++ main.go
@@ -8,31 +8,12 @@ import (
 	"time"
 )
 
-// func checkAnswer(answer string, correctAnswer string) (correct, incorrect int) {
-// 	correct = 0
-// 	incorrect = 0
-// 	if answer == correctAnswer {
-// 		correct++
-// 	} else {
-// 		incorrect++
-// 	}
-// 	return correct, incorrect
-// }
-
-type Pair struct {
-	First  string
-	Second int
-}
-
-func userInput(records [][]string, ch chan Pair) {
-	for i := 1; i <= 107; i++ {
-		fmt.Printf("%v\n", records[i][0])
-		var answer string
-		fmt.Scan(&answer)
-		pair := Pair{First: answer, Second: i}
-		ch <- pair
-		// TODO RESET TIMER AFTER EACH INPUT
-	}
+func userInput(question string, done chan bool, ch chan string) {
+	fmt.Printf("%v\n", question)
+	var answer string
+	fmt.Scan(&answer)
+	done <- true
+	ch <- answer
 }
 
 func main() {
@@ -53,32 +34,33 @@ func main() {
 		log.Fatal(error)
 	}
 
-	// fmt.Printf("%T", records)
-	ch := make(chan Pair)
-
-	var limit int = 5
-	waitingTime := time.Duration(limit) * time.Second
-	fmt.Printf("Time limit is %v\n", waitingTime)
-
-	go userInput(records, ch)
-
+	timeout := 5 * time.Second
+	ch := make(chan string)
 	var correct int
 	var incorrect int
-	select {
-	case input := <-ch:
-		answer := input.First
-		i := input.Second
-		if records[i][1] == answer {
-			correct++
-		} else if answer == "break" {
-			break
-		} else {
-			incorrect++
-		}
 
-	case <-time.After(waitingTime):
-		fmt.Println("Timeout!")
-		os.Exit(1)
+	for i := 1; i <= 107; i++ {
+		done := make(chan bool)
+		go userInput(records[i][0], done, ch)
+
+		select {
+		case <-done:
+			answer := <-ch
+			if records[i][1] == answer {
+				correct++
+			} else if answer == "Exit" {
+				fmt.Println("YOU EXITED!")
+				fmt.Printf("correct answers %v\nwrong answers %v\nunattempted questions %v\n", correct, incorrect, 107-(correct+incorrect))
+				os.Exit(1)
+			} else {
+				incorrect++
+			}
+
+		case <-time.After(timeout):
+			fmt.Println("Timeout!")
+			fmt.Printf("correct answers %v\nwrong answers %v\nunattempted questions %v\n", correct, incorrect, 107-(correct+incorrect))
+			os.Exit(1)
+		}
 	}
-	fmt.Printf("correct answers %v\nwrong answers %v\nunattempted questions %v\n", correct, incorrect, 107-(correct+incorrect))
+
 }
